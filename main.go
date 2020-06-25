c29acf03b4a54f9a27bb4f29ba57b693c436c896
Andrew Tang <atreeleaf@gmail.com>
2020-06-25 13:59:34 -0700

    added infura connection

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
 	"github.com/gorilla/mux"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
@@ -13,13 +13,19 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fmt.Println("Congrats, the build ran Successfully!")
-	fmt.Println("Now running server on 8080...")
+	client, err := ethclient.Dial("https://mainnet.infura.io")
+	if err != nil {
+		log.Fatalf("Unable to connect")
+	}
+	_ = client
+	fmt.Println("Successfully connected to Infura/Ethereum")
 
 	router := mux.NewRouter().StrictSlash(true)
 
 	// @Travis: Declare what our "/" endpoint will do.
 	router.HandleFunc("/", homeLink)
+	fmt.Println("Congrats, the build ran Successfully!")
+	fmt.Println("Now running server on 8080...")
 
 	// @Travis: Log all requests from -> Server started at port 8080, localhost.
 	// Uses router declaration as the "servlet" equivalent
