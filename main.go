743af6f8cd5b35116b363708958ce890375209ed
zeroviral <kshari7@msn.com>
2020-06-25 15:33:42 -0700

    Removing zombie code.

--- main.go
+++ main.go
@@ -16,13 +16,6 @@ func main() {
 	// Connect to ethereum network through infura
 	utils.ConnectClient(connectionURL)
 
-	//account := common.HexToAddress("0x742d35cc6634c0532925a3b844bc454e4438f44e")
-
-	//balance, err := client.BalanceAt(context.Background(), account, nil)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Printf("The balance of this ethereum address is: %s ether \n", balance)
 	router := mux.NewRouter().StrictSlash(true)
 
 	// @Travis: Declare what our "/" endpoint will do.
