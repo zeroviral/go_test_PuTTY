852cbc8a5c0db03dc2a88af80439a8893e03f6f0
Andrew Tang <atreeleaf@gmail.com>
2020-06-27 22:10:57 -0700

    updated

--- resources/schemas.go
+++ resources/schemas.go
@@ -4,7 +4,7 @@ package resources
 type EthereumRequest struct {
 
 	// The field that will be accepted on the left, and the right is the response formatted fields.
-	EID string `json:"eID" validate:"required"`
+	EID string `json:"eID" validate:"required,gt=38,lt=43"`
 }
 
 // This holds our definition of a list that can hold multiple ethereum requests.
