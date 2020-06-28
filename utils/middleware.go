84c08a9d5261aed3f36a68f945da67f2767a39c9
Andrew Tang <atreeleaf@gmail.com>
2020-06-27 18:18:07 -0700

    added context propogation to middleware

--- utils/middleware.go
+++ utils/middleware.go
@@ -1,14 +1,19 @@
 package utils
 
 import (
+	"context"
+	"fmt"
 	"github.com/google/uuid"
 	"net/http"
 )
 
-func LogRequestMetaData(next http.Handler) http.Handler{
-	transactionID := uuid.New()
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-		LogInfo.Printf("[%s]: %s %s request received", transactionID, r.RequestURI, r.Method)
-		next.ServeHTTP(w,r)
+func LogRequestMetaData(next http.Handler) http.Handler {
+	txID := uuid.New()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, "transactionID", txID)
+		r = r.WithContext(ctx)
+		LogInfo.Printf("%s %s request received transactionID:[%s]", r.RequestURI, r.Method, txID)
+		next.ServeHTTP(w, r)
 	})
 }
