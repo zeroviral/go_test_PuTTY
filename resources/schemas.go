1a330f8f87b7faccc450183367e7f891b8c9fe82
Andrew Tang <atreeleaf@gmail.com>
2020-06-28 15:44:29 -0700

    using function receivers to validate parent struct

--- resources/schemas.go
+++ resources/schemas.go
@@ -1,5 +1,14 @@
 package resources
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"go_test_PuTTY/utils"
+	"net/http"
+	"strings"
+)
+
 // This will conform our request structure.
 type EthereumRequest struct {
 
@@ -13,4 +22,29 @@ type EthereumRequestList []EthereumRequest
 type EthereumResponse struct {
 	EthereumId string `json:"eID"`
 	Message    string `json:"message"`
+}
+
+
+func (e *EthereumRequest) ValidateJSON (req *http.Request) error {
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(e)
+	if err != nil {
+		return fmt.Errorf("Incorrect JSON format")
+	}
+	return nil
+}
+
+func (e *EthereumRequest) ValidateSchema () error {
+	validate := validator.New()
+	err := validate.Struct(e)
+	if err != nil {
+		var errors strings.Builder
+		for _, err := range err.(validator.ValidationErrors) {
+			utils.LogError.Printf("%v\n", err)
+			errors.WriteString(err.Field())
+		}
+		return fmt.Errorf("error on schema validation %s", errors.String())
+	}
+	return nil
 }
--- validators/common.go
+++ validators/common.go
@@ -1,11 +1,8 @@
 package validators
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/go-playground/validator/v10"
 	"go_test_PuTTY/resources"
-	"go_test_PuTTY/utils"
 	"net/http"
 	"strings"
 )
@@ -13,35 +10,15 @@ import (
 // TODO: Decouple JSON parse validation logic with schema validation.
 func ValidateRequest(req *http.Request) (resources.EthereumRequest, error) {
 	// JSON parsing Validation
-	var validRequest resources.EthereumRequest
-	dec := json.NewDecoder(req.Body)
-	// This will disallow any fields not defined in the schema, down to the tags.
-	dec.DisallowUnknownFields()
-	// Now just put the decoded JSON into the request format.
-	err := dec.Decode(&validRequest)
-	if err != nil {
-		return validRequest, fmt.Errorf("incorrect request format. please check your inputs")
+	var ethereumRequest resources.EthereumRequest
+	if err := ethereumRequest.ValidateJSON(req); err != nil {
+		return ethereumRequest, fmt.Errorf("incorrect request format. please check your inputs")
 	}
 	// Schema enforcement validation
-	if _, err = ValidateSchema(validRequest); err != nil  {
-		return validRequest, err
+	if err := ethereumRequest.ValidateSchema(); err != nil  {
+		return ethereumRequest, err
 	}
-	return validRequest, nil
-}
-
-func ValidateSchema(validRequest interface{}) (bool, error) {
-
-	validate := validator.New()
-	err := validate.Struct(validRequest)
-	if err != nil {
-		var errors strings.Builder
-		for _, err := range err.(validator.ValidationErrors) {
-			utils.LogError.Printf("%v\n", err)
-			errors.WriteString(err.Field())
-		}
-		return false, fmt.Errorf("error on schema validation %s", errors.String())
-	}
-		return true, nil
+	return ethereumRequest, nil
 }
 
 // Validates a request is of a valid expected action.
