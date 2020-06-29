745e12e196970d7b8425daaaf603adb892409c6f
zeroviral <kshari7@msn.com>
2020-06-28 17:25:28 -0700

    Chopping up the Transactions GET handler into modular pieces.

--- controllers/transactions.go
+++ controllers/transactions.go
@@ -13,10 +13,23 @@ import (
 // ENDPOINT: ("/transactions")
 // ACCEPTS: GET_Transactions
 func TransactionsGET(h http.ResponseWriter, req *http.Request) error {
+
+	// Offload the bulk of the validation initially.
 	validRequest, err := validate.TransactionsGET(req)
 	if err != nil {
 		return utils.NewError4XX(err, 405, err.Error())
 	}
+	response := getAndFillAccountInfo(validRequest)
+	// Finally, write the new payload to the response object.
+	if err = json.NewEncoder(h).Encode(response); err != nil {
+		return fmt.Errorf("Unable to encode JSON response: %v", err)
+	}
+	return nil
+}
+
+// Helper method. Uses a valid TRANSACTION GET request to create and update the EthereumResponse object.
+// TODO: We should probably include some error handling here and beef up the logic if they throw us errors.
+func getAndFillAccountInfo(validRequest resources.TransactionsSchemaGET) resources.EthereumResponse {
 	// convert to correct format
 	account := common.HexToAddress(validRequest.EID)
 	// get balance
@@ -26,11 +39,7 @@ func TransactionsGET(h http.ResponseWriter, req *http.Request) error {
 		EthereumId: validRequest.EID,
 		Message:    fmt.Sprintf("The current Etehreum balance is %s", balance),
 	}
-	// Finally, write the new payload to the response object.
-	if err = json.NewEncoder(h).Encode(response); err != nil {
-		return fmt.Errorf("Unable to encode JSON response: %v", err)
-	}
-	return nil
+	return response
 }
 
 
